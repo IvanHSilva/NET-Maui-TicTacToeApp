6e4fc54ade4d78e6a5d8a9d21ab62e7aca59f625
IvanHS <ivanhenriquessilva@gmail.com>
2024-12-10 21:26:50 -0300

    Game rules test

--- TicTacToeApp/MainPage.xaml.cs
+++ TicTacToeApp/MainPage.xaml.cs
@@ -11,6 +11,7 @@
         private void Button_Clicked(object sender, EventArgs e) {
 
             Button button = (Button)sender;
+            button.IsEnabled = false;
             
             if (simbol == "X") {
                 button.Text = "X";
@@ -28,14 +29,23 @@
 
         void Reset() {
             S1.Text = "";
+            S1.IsEnabled = true;
             S2.Text = "";
+            S2.IsEnabled = true;
             S3.Text = "";
+            S3.IsEnabled = true;
             S4.Text = "";
+            S4.IsEnabled = true;
             S5.Text = "";
+            S5.IsEnabled = true;
             S6.Text = "";
+            S6.IsEnabled = true;
             S7.Text = "";
+            S7.IsEnabled = true;
             S8.Text = "";
+            S8.IsEnabled = true;
             S9.Text = "";
+            S9.IsEnabled = true;
         }
     }
 }
